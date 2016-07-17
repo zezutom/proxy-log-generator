9ef1c941a329842f2bf65943fe738d69923f990e
tomaszezula <tomas.zezula@panaseer.com>
2016-07-17 22:38:53 +0100

    Enhanced randomised URLs with fake resource paths. Authentication details made optional. Finally, date/time truncated to seconds.

--- src/log_generator.py
+++ src/log_generator.py
@@ -7,13 +7,13 @@ from random import randrange
 
 
 def generate_event(timestamp):
-    logging.info('%s\t%s\t%s\t%s\t%s\t%s\t%s' % (timestamp,
-                                           rand_ip(),
-                                           rand_user_agent(),
-                                           rand_auth(),
-                                           rand_url(),
-                                           rand_http_status(),
-                                           rand_res_size()))
+    logging.info('%s\t%s\t%s\t%s\t%s\t%s\t%s' % (timestamp.strftime('%Y-%m-%d %H:%M:%S'),
+                                                 rand_ip(),
+                                                 rand_user_agent(),
+                                                 rand_auth(),
+                                                 rand_url(),
+                                                 rand_http_status(),
+                                                 rand_res_size()))
 
 
 def generate_log(options):
@@ -22,14 +22,17 @@ def generate_log(options):
     timestamp = current_time - timedelta(hours=options.duration)
     while current_time > timestamp:
         generate_event(timestamp)
-        timestamp = timestamp + timedelta(minutes=options.increment)
+        timestamp += timedelta(minutes=options.increment, seconds=randrange(0, 59))
 
 
 def read_options():
     parser = OptionParser()
-    parser.add_option('-f', '--file', dest='logfile', help='Path to a log file. Default=logfile.log', default='logfile.log', type='string')
-    parser.add_option('-t', '--time', dest='duration', help='Generate logs for X Hours. Default=48H ( 2 days)', default='48', type='int')
-    parser.add_option('-i', '--increment', dest='increment', help='Generate logs every X minutes. Default=5min', default='5', type='int')
+    parser.add_option('-f', '--file', dest='logfile', help='Path to a log file. Default=logfile.log',
+                      default='logfile.log', type='string')
+    parser.add_option('-t', '--time', dest='duration', help='Generate logs for X Hours. Default=48H ( 2 days)',
+                      default='48', type='int')
+    parser.add_option('-i', '--increment', dest='increment', help='Generate logs every X minutes. Default=5min',
+                      default='5', type='int')
     (options, args) = parser.parse_args()
     return options
 
@@ -51,28 +54,38 @@ def rand_user_agent():
     return rand_item(user_agents)
 
 
-# TODO not all requests are necessarily authenticated
 def rand_auth():
+    if not rand_bool():
+        return '-'
     fnames = read_conf('fnames.txt')
     lnames = read_conf('lnames.txt')
     return ('.'.join([rand_item(fnames), rand_item(lnames)])).lower()
 
 
 def read_conf(filename):
-    return open('conf/%s'%filename, 'r').readlines()
+    return open('conf/%s' % filename, 'r').readlines()
 
 
 def rand_bool():
     return bool(random.getrandbits(1))
 
 
-# TODO add random resource links (images, html pages etc.)
 def rand_url():
     first = ':'.join(['https' if rand_bool() else 'http', '//www'])
     fname = rand_item(read_conf('fnames.txt'))
     lname = rand_item(read_conf('lnames.txt'))
     delimiter = '-' if rand_bool() else ''
-    return '.'.join([first, ('%s%s%s' % (fname, delimiter, lname)).lower(), 'com'])
+    base_url = '.'.join([first, ('%s%s%s' % (fname, delimiter, lname)).lower(), 'com'])
+    return ''.join([base_url, rand_resource()])
+
+
+def rand_resource():
+    depth = randrange(1, 5)
+    suffixes = ['jpg', 'png', 'html', 'xml', 'php', 'asp']
+    resource = ''
+    for i in range(depth):
+        resource = '/'.join([resource, str(randrange(1, 999))])
+    return '.'.join([resource, rand_item(suffixes)])
 
 
 def rand_http_status():
@@ -81,14 +94,16 @@ def rand_http_status():
 
 # up until 5MB
 def rand_res_size():
-    return random.randrange(1, 5000000)
+    return randrange(1, 5000000)
 
-def rand_item(list):
-    return str(list[randrange(0, len(list) - 1)]).strip()
+
+def rand_item(custom_list):
+    return str(custom_list[randrange(0, len(custom_list) - 1)]).strip()
 
 
 def main():
     generate_log(options=read_options())
+
 
 if __name__ == '__main__':
     main()
